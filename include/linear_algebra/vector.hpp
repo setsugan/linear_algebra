851acbb1f3d73f1fa05b212a052063784889465a
riku yukimaru <setsugan.robocon@gmail.com>
2026-04-09 20:06:55 +0900

    feat: add xyz components

--- include/linear_algebra/vector.hpp
+++ include/linear_algebra/vector.hpp
@@ -1,4 +1,22 @@
 #ifndef LINEAR_ALGEBRA_VECTOR_HPP
 #define LINEAR_ALGEBRA_VECTOR_HPP
 
+#include <iostream>
+#include <cmath>
+#include <assert.h>
+
+namespace linear_algebra {
+class Vector {
+private:
+    double x_;
+    double y_;
+    double z_;
+public:
+    Vector() = default;
+    Vector(double x, double y, double z) : x_(x), y_(y), z_(z) {};
+    ~Vector() = default;
+};
+
+} // namespace linear_algebra
+
 #endif // LINEAR_ALGEBRA_VECTOR_HPP
